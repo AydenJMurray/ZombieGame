7adfe3ead726919d80d08e615e9201377672f941
conormcharg <conormcharg@live.co.uk>
2016-03-09 18:04:22 +0000

    Started home and login pages.

--- zombie_proj/Zombies/views.py
+++ zombie_proj/Zombies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'Zombies/Home.html',{})
+    
+    
+def login(request):
+    return HttpResponse("Login page")
--- zombie_proj/zombie_proj/settings.py
+++ zombie_proj/zombie_proj/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIRS = (TEMPLATE_PATH,)
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Zombies',
 )
 
 MIDDLEWARE_CLASSES = (
