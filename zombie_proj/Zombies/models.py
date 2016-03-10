154bdf05a4289f25bb39107e5ebae085fda3d2d8
JamieSweeney321 <321.jamiesweeney.321@gmail.com>
2016-03-10 02:24:34 +0000

    Update models.py

--- zombie_proj/Zombies/models.py
+++ zombie_proj/Zombies/models.py
@@ -2,44 +2,43 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = model.CharField(max_length=128, unique=True)
-    password = model.CharField(max_length=64)
-    email = model.EmailField(max_length=75)
+    username = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=64)
+    email = models.EmailField(max_length=75)
     def __unicode__(self):
         return self.username
 
 
 class Player(models.Model):
-    user = model.OneToOneField('User', unique = True)
-    profile_picture = model.ImageField(height_field=none, width_field=none ,max_length=100)
-    games_played = model.IntegerField()
-    most_days_survived = model.IntegerField()
-    most_kills = model.IntegerField()
-    most_people = model.IntegerField()
-    avg_days = model.DecimalField(max_digits = 5, decimal_places = 1)
-    avg_kills = model.DecimalField(max_digits = 5, decimal_places = 1)
-    avg_people = model.DecimalField(max_digits = 5, decimal_places = 1)
-    current_game = model.FileField(max_length = 100)
+    user = models.OneToOneField('User', unique = True)
+    profile_picture = models.ImageField(max_length=100)
+    games_played = models.IntegerField()
+    most_days_survived = models.IntegerField()
+    most_kills = models.IntegerField()
+    most_people = models.IntegerField()
+    avg_days = models.DecimalField(max_digits = 5, decimal_places = 1)
+    avg_kills = models.DecimalField(max_digits = 5, decimal_places = 1)
+    avg_people = models.DecimalField(max_digits = 5, decimal_places = 1)
+    current_game = models.FileField(max_length = 100)
     def __unicode__(self):
         return self.user.username
 
 class Badge(models.Model):
-    name = model.CharField(max_length=128)
-    description = model.CharField(max_length=64)
-    criteria = model.IntegerField()
-    badge_type = model.CharField(max_length = 10)
-    level = model.CharField(max_length = 10)
-    icon = model.ImageField(height_field=none, width_field=none ,max_length=100)
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=64)
+    criteria = models.IntegerField()
+    badge_type = models.CharField(max_length = 10)
+    level = models.CharField(max_length = 10)
+    icon = models.ImageField(max_length=100)
     
     class Meta:
         unique_together = (('name','level'))
 
 
-class Acheivement(models.Model):
-    player = model.ForeignKey('Player')
-    badge = model.ForeignKey('Badge')
-    date_awarded = model.TimeField(auto_now_add=True)
+class Achievement(models.Model):
+    player = models.ForeignKey('Player')
+    badge = models.ForeignKey('Badge')
+    date_awarded = models.TimeField(auto_now_add=True)
     class Meta:
         unique_together = (('player','badge'))
-
 
