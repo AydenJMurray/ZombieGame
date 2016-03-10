1222b3ece85d071803d0582ef92f2f082d95f492
kennymurr <kennymurr10@gmail.com>
2016-03-10 02:25:18 +0000

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from Zombies.models import User, Player, Badge, Achievement
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Player)
-admin.site.register(Badge)
-admin.site.register(Achievement)
