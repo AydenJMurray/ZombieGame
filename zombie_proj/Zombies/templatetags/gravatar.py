9f8ec63ec7aa5a385fa2846ade8c07a8e65a932e
JamieSweeney321 <321.jamiesweeney.321@gmail.com>
2016-03-17 21:39:55 +0000

    Update gravatar.py
    added default picture

--- zombie_proj/Zombies/templatetags/gravatar.py
+++ zombie_proj/Zombies/templatetags/gravatar.py
@@ -9,7 +9,7 @@ register = template.Library()
 # TEMPLATE USE:  {{ email|gravatar_url:150 }}
 @register.filter
 def gravatar_url(email, size=40):
-  default = "http://example.com/static/images/defaultavatar.jpg" #not working yet
+  default = "https://heatherchristenaschmidt.files.wordpress.com/2011/09/facebook_no_profile_pic2-jpg.gif"
   return "http://www.gravatar.com/avatar/%s?%s" % (hashlib.md5(email.lower()).hexdigest(), urllib.urlencode({'d':default, 's':str(size)}))
  
 # return an image tag with the gravatar
