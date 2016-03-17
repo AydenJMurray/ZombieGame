337c8148b725f3b2c94dd0ad997ef3c37333bf57
JamieSweeney321 <321.jamiesweeney.321@gmail.com>
2016-03-17 21:01:25 +0000

    Update views.py
    forgot to add the import stuff, imported 404 and user

--- zombie_proj/Zombies/views.py
+++ zombie_proj/Zombies/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from Zombies.forms import UserForm, PlayerForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from Zombies.models import Player
+from django.contrib.auth.models import User
+
 from scripts.main import *
 
 # Create your views here.
