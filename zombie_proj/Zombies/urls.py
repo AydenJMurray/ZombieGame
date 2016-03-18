d78831c6489dc07362a7bb11f0fd537e2e438f4b
Ayden Murray <ayden.murray.97@gmail.com>
2016-03-18 20:19:38 +0000

    Removed obsolete urls, redirect 'Play' when logged in to the start page, not game_page

--- zombie_proj/Zombies/urls.py
+++ zombie_proj/Zombies/urls.py
@@ -3,9 +3,6 @@ from Zombies import views
 
 urlpatterns = patterns('',
         url(r'^$', views.home, name='home'),
-        url(r'^register/$', views.register, name='register'),
-        url(r'^login/$', views.user_login, name='login'),
-        url(r'^logout/$', views.user_logout, name='logout'),
         url(r'^game_page/$', views.game_page, name='game_page'),
         url(r'^leaderboards/$', views.leaderboard, name='leaderboards'),
         url(r'^start/', views.start, name = 'start'),
--- zombie_proj/Zombies/views.py
+++ zombie_proj/Zombies/views.py
@@ -31,62 +31,6 @@ def leaderboard(request):
 
      context_dict={'kills':kills, 'days':days, 'people':people, 'allKills':allKills,'allDays':allDays ,'allPeople':allPeople }
      return render(request, 'Zombies/leaderboards.html', context_dict)
-
-
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user:
-            if user.is_active:
-                login(request,user)
-                return HttpResponseRedirect('/Zombies/')
-            else:
-                return HttpResponse("Your Zombie account is disabled.")
-        else:
-            print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
-    else:
-        return render(request, 'Zombies/login.html',{})
-    
-def register(request):
-    registered = False
-    
-    if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
-        player_form = PlayerForm(data=request.POST)
-        
-        if user_form.is_valid() and player_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-            player = player_form.save()
-            player.user = user
-            
-            if 'picture' in request.FILES:
-                player.profile_picture = request.FILES['profile_picture']
-            player.save()
-            registered = True
-        else:
-            print user_form.errors,player_form.errors
-    else:
-        user_form = UserForm()
-        player_form = PlayerForm()
-        
-    return render(request,
-    'Zombies/registration_form.html',
-    {'user_form': user_form, 'player_form':player_form ,'registered':registered})
-
-
-@login_required
-def user_logout(request): 
-    logout(request)
-    return HttpResponseRedirect('/Zombies/')
-    
-def game_page(request):
-    return render(request, 'Zombies/game_page.html',{})
-
     
 def start(request):
     g = Game()
