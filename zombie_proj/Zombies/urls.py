6c2f6b8c8cdd0225ffaefee318f4880c11be2474
kennymurr <kennymurr10@gmail.com>
2016-03-25 23:54:30 +0000

    Update urls.py

--- zombie_proj/Zombies/urls.py
+++ zombie_proj/Zombies/urls.py
@@ -8,7 +8,6 @@ urlpatterns = patterns('',
         url(r'^turn/(?P<turn>[A-Z]+)/(?P<pos>[0-9]+)/', views.turn, name = 'turn'),
         url(r'^user/(?P<user_name>[A-Za-z_/,-\.0-9]+)/$', views.userProfile, name = 'userProfile' ),
         url(r'^edit/$', views.edit_badges, name = 'badgeEditor'),
-        url(r'^add/$', views.add_user, name='addUser'),
         url(r'^new/$', views.new, name = 'newGame'),
         url(r'^help/$', views.howto, name = 'help'),
         url(r'^player404/$', views.player404, name = 'player404')
